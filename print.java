807cf3a238f9d1eedbdc93e562443d81e9782480
alfred <ogzialfred@gmail.com>
2023-03-20 16:52:23 +0300

    Java Proj 101-1

---
+++ print.java
@@ -0,0 +1,6 @@
+public class print{
+    public static void main(String[] args){
+
+        System.out.println("printing function");//this is the code that prints something to the terminal
+    }
+}
