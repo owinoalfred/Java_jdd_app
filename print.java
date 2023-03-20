c868ae962cf8d8b716497ca489de45d2b2579a11
alfred <ogzialfred@gmail.com>
2023-03-20 17:06:53 +0300

    JVM JRE JVM congigs

--- print.java
+++ print.java
@@ -1,6 +1,8 @@
 public class print{
     public static void main(String[] args){
-
+        int age =30;
+        age =age+  50;
         System.out.println("printing function");//this is the code that prints something to the terminal
+        System.out.println(age);
     }
 }
