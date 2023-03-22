fc98817a6832e88f3c69a88ec576e08f6e550b58
alfred <ogzialfred@gmail.com>
2023-03-22 11:28:57 +0300

    arrays.deep.tostring()

--- javaMainFiles/arrays.java
+++ javaMainFiles/arrays.java
@@ -6,6 +6,9 @@ public class arrays {
     public static void main(String[] args){
         int[] numbersArray = {4,5,4,3,3};
         System.out.println(Arrays.toString(numbersArray));
+        //2d arrays
+        int[][] twoDarray = {{44,56,5}, {54,5,4}, {34,67,6}};
+        System.out.println(Arrays.deepToString(twoDarray));
 
     }
     
