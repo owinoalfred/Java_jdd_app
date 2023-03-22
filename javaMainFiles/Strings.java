15ec51ce724ebbd87a28cce3e40bb33b47d08e39
alfred <ogzialfred@gmail.com>
2023-03-22 11:21:14 +0300

    2d 3d arrays

--- javaMainFiles/Strings.java
+++ javaMainFiles/Strings.java
@@ -4,5 +4,10 @@ public class Strings{
     public static void main(String[] args){
         String message = new String("Hello People please come and get th Ids");
         System.out.println(message.replace("e", "o"));
+        System.out.println(message.toUpperCase());
+        String windowss = new String("c:\\windows\\Desktop\\");    
+        System.out.println(windowss);
+
+
     }
 }
---
+++ javaMainFiles/arrays.java
@@ -0,0 +1,12 @@
+package javaMainFiles;
+
+import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[] args){
+        int[] numbersArray = {4,5,4,3,3};
+        System.out.println(Arrays.toString(numbersArray));
+
+    }
+    
+}
