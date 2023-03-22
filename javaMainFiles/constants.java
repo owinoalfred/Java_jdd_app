fce91df4682a04d987d04e824db0b9d8bc0e6a36
alfred <ogzialfred@gmail.com>
2023-03-22 11:36:09 +0300

    aarithmetic oppps

---
+++ javaMainFiles/constants.java
@@ -0,0 +1,14 @@
+package javaMainFiles;
+
+public class constants {
+    public static void main(String[] args){
+        int result =  10 + 3;
+        double result2 = (double)10/(double)3;
+        System.out.println(result2);
+        int x = ++result;
+        System.out.println(x);
+        x =result++;
+        System.out.println(x);
+    }
+    
+}
