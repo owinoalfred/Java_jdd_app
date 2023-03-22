14d109eb027822c013b54983f41df1a9ac2b8331
alfred <ogzialfred@gmail.com>
2023-03-22 10:34:24 +0300

    Date being updated

---
+++ javaMainFiles/Main.java
@@ -0,0 +1,14 @@
+package javaMainFiles;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+        int age; 
+        int year;
+        Date todaysDate = new Date();
+        
+        System.out.println("this is Java code");
+        System.out.println(todaysDate);
+    }
+}
--- print.java
+++ print.java
@@ -1,8 +1,26 @@
+import java.util.Date;
+
 public class print{
     public static void main(String[] args){
         int age =30;
         age =age+  50;
+        int new_age = age;
+        long viewsCount = 6_123_456_789L;
         System.out.println("printing function");//this is the code that prints something to the terminal
         System.out.println(age);
+        System.out.println(age);
+        byte gigaByte = 5;
+        //these are primitive types
+        float CGPA = 10.99F;
+        char alphabet = 'e';
+        boolean isEligible = true;
+        boolean notEligible = false;
+        
+        //there are premitive types
+        Date todaysDate = new Date();
+        System.out.println( new_age);
+        System.out.println(gigaByte);
+        System.out.println(viewsCount);
+        System.out.println(todaysDate);
     }
 }
