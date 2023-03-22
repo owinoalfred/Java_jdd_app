96842ff65aec87ca9a4157cd3dac9760bdc1c01b
alfred <ogzialfred@gmail.com>
2023-03-22 11:03:58 +0300

    Letters modification

--- javaMainFiles/Main.java
+++ javaMainFiles/Main.java
@@ -1,14 +1,21 @@
 package javaMainFiles;
 
+import java.awt.Point;
 import java.util.Date;
 
+
 public class Main {
+    private static int x;
+    private static int y;
+
     public static void main(String[] args){
         int age; 
         int year;
         Date todaysDate = new Date();
-        
         System.out.println("this is Java code");
         System.out.println(todaysDate);
+        Point point1 = new Point(x, y);
+        point1.x = 3;
+        point1.y=10;
     }
 }
---
+++ javaMainFiles/Strings.java
@@ -0,0 +1,8 @@
+package javaMainFiles;
+
+public class Strings{
+    public static void main(String[] args){
+        String message = new String("Hello People please come and get th Ids");
+        System.out.println(message.replace("e", "o"));
+    }
+}
