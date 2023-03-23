d47a81ee3fb6bfc18008b342bcee355f415fc09c
alfred <ogzialfred@gmail.com>
2023-03-23 09:47:42 +0300

    tenarry operators!

---
+++ tenaries.java
@@ -0,0 +1,9 @@
+public class tenaries {
+    public static void main(String[] args){
+        boolean male = true;
+        boolean notMale = false;
+        String pronoun = male ? "Mr" : "Mrs";
+        System.out.println(pronoun);
+    }
+    
+}
