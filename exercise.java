91aeb1bb590f0364cb6b60d8ef03ad835f2193e8
alfred <ogzialfred@gmail.com>
2023-03-23 10:26:22 +0300

    FIzzBuss

---
+++ exercise.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercise{
+    public static void main(String[] aargs){
+        Scanner scannedNumber = new Scanner(System.in);
+        System.out.print("please input a number: ");
+        int userInput = scannedNumber.nextInt();
+
+        if (userInput % 3 == 0 && userInput % 5 == 0){
+            System.out.println("FIZZBUZZ");
+        }
+        else if(userInput % 3 == 0){
+            System.out.println("BUZZ");
+        }
+        else if (userInput%5==0){
+            System.out.println("FIZZ");
+        }
+        else{
+            System.out.println(userInput);
+        }
+        
+    }
+}
