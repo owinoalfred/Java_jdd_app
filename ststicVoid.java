5224b8348df5e81d3dd712f5efc34ad2b4539687
alfred <ogzialfred@gmail.com>
2023-03-25 23:35:00 +0300

    while loops

--- ststicVoid.java
+++ ststicVoid.java
@@ -1,7 +1,12 @@
 public class ststicVoid {
     public static void main(String[] args){
-
-
+    int age = 66;
+    int currency = 999_454_444;
+    boolean yes = true;
+    String name = new String("THIS IS THE MAIN NAMAE");
+    System.out.println("this is the in: "+ name+ age);
+    for (int i=0; i < len(name); i++){
         
+    }
     }
 }
