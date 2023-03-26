6bc1d467cce420fafb680dd2f03b2300efb773e7
alfred <ogzialfred@gmail.com>
2023-03-26 11:28:14 +0300

    Compare the elements

--- javaMainFiles/src2/SRC2/Main.java
+++ javaMainFiles/src2/SRC2/Main.java
@@ -3,7 +3,8 @@ package SRC2;
 
 public class Main{
     public static void main(String[] args){
-
-        
+        int first = 60;
+        int second = 61;
+        System.out.println(first==second);
     }
 }
