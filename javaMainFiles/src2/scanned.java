0fd8ae2cf7153235bbac2e49a25acba4dbc61ad6
alfred <ogzialfred@gmail.com>
2023-03-27 10:15:58 +0300

    Compare the elements

---
+++ javaMainFiles/src2/scanned.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class scanned{
+    public static void main(String[] args){
+
+        Scanner newName = new Scanner(System.in);
+        System.out.print("What is your name: "+ newName);
+        int dividerNumber = 56;
+        Scanner firstName = new Scanner(System.in);
+        String name = firstName.next();
+        System.out.println("WOW your name is : " + firstName);
+
+        }
+}
