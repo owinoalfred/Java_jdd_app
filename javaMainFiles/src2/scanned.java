044d31c3be0be19e82d6f115b8069367005866a4
alfred <ogzialfred@gmail.com>
2023-03-27 10:36:49 +0300

    the scanner func

--- javaMainFiles/src2/scanned.java
+++ javaMainFiles/src2/scanned.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class scanned{
     public static void main(String[] args){
 
-        Scanner newName = new Scanner(System.in);
-        System.out.print("What is your name: "+ newName);
-        int dividerNumber = 56;
-        Scanner firstName = new Scanner(System.in);
-        String name = firstName.next();
-        System.out.println("WOW your name is : " + firstName);
+       Scanner name = new Scanner(System.in);
+       String firsname = name.next();
+       System.out.println("your firt name is : " + firsname);
 
+       Scanner lastname = new Scanner(System.in);
+       String nameLast = lastname.nextLine();
+       System.out.println("your name is : " + nameLast);
         }
 }
